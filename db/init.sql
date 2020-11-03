e18eddf4365b2b0d47a07b10f33991393cb074fe
Máhonfai Bálint <mbalint314@gmail.com>
2020-11-03 18:04:51 +0100

    feature: add database initialization script

---
+++ db/init.sql
@@ -0,0 +1,35 @@
+CREATE TABLE sensor
+(
+    id   INT NOT NULL AUTO_INCREMENT,
+    name NVARCHAR(100),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE sensor_data
+(
+    id        INT NOT NULL AUTO_INCREMENT,
+    sensor_id INT NOT NULL,
+    timestamp INT NOT NULL,
+    data      INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (sensor_id) REFERENCES sensor (id)
+);
+
+CREATE TABLE plant
+(
+    id   INT NOT NULL AUTO_INCREMENT,
+    name NVARCHAR(100),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE requirement
+(
+    id        INT NOT NULL AUTO_INCREMENT,
+    plant_id  INT NOT NULL,
+    sensor_id INT NOT NULL,
+    min_value INT NOT NULL,
+    max_value INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (plant_id) REFERENCES plant (id),
+    FOREIGN KEY (sensor_id) REFERENCES sensor (id)
+);
