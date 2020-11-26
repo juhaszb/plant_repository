e09eccf9e81f26e65992d6bd6b54733e5c459fea
Máhonfai Bálint <mbalint314@gmail.com>
2020-11-26 19:57:11 +0100

    fix: fix if

--- simulation/actor.py
+++ simulation/actor.py
@@ -3,11 +3,11 @@ import bottle
 import requests
 
 
-@bottle.post("/send/<action:int>")
+@bottle.post("/send/<action:int>/")
 def send(action):
     if action == 1:
         print("Action request received: increase")
-    if action == -1:
+    elif action == -1:
         print("Action request received: decrease")
     else:
         bottle.abort(400)
@@ -22,9 +22,10 @@ def main():
     parser.add_argument("--sensor-id", type=int, required=True)
     args = parser.parse_args()
 
-    requests.post(
-        f"{args.server_url}/register/{args.id}/{args.sensor_id}", data=str(args.port)
+    r = requests.put(
+        f"{args.server_url}/register/{args.id}/{args.sensor_id}/", data=str(args.port)
     )
+    print(r.text)
 
     bottle.run(host=args.host, port=args.port)
 
